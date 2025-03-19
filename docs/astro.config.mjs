ab1e6029f2727aeace73676a086217fe414e497f
lutzgo <lutz0go@gmail.com>
2025-03-19 14:40:23 +0100

    Fix: Pass an empty options object into starlight() in astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  integrations: [starlight()],
+  integrations: [starlight({})],
 });
